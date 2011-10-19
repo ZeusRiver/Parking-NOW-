3d2aad0fb3b5712491b862d3a5db470e8347a3b0
Raymond Kao <ray@raymondkao.com>
2011-10-19 10:46:05 -0400

    Added haml and sass plugin

---
+++ _plugins/haml_converter.rb
@@ -0,0 +1,44 @@
+module Jekyll
+  require 'haml'
+  class HamlConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /haml/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      engine = Haml::Engine.new(content)
+      engine.render
+    end
+  end
+
+  require 'sass'
+  class SassConverter < Converter
+    safe true
+    priority :low
+
+     def matches(ext)
+      ext =~ /scss/i
+    end
+
+    def output_ext(ext)
+      ".css"
+    end
+
+    def convert(content)
+      begin
+        puts "Performing Sass Conversion."
+        engine = Sass::Engine.new(content, :syntax => :scss)
+        engine.render
+      rescue StandardError => e
+        puts "!!! SASS Error: " + e.message
+      end
+    end
+  end
+end
